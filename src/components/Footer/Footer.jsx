f67a30c8fbf9060a0b73c5f7808daa6dc19a4e15
raenllanthos <cwdarksouls@gmail.com>
2021-07-22 21:07:37 -0400

    majority changed, just need to add capstone

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,16 +8,14 @@ import { githubButtons } from '../../mock/data';
 
 const Footer = () => {
   const { footer } = useContext(PortfolioContext);
-  const { networks } = footer;
+  const { btn, networks } = footer;
   const { isEnabled } = githubButtons;
 
   return (
     <footer className="footer navbar-static-bottom">
       <Container>
-        <span className="back-to-top">
-          <Link to="hero" smooth duration={1000}>
-            <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
-          </Link>
+        <span className="cta-btn2 cta-btn--footer">
+          <Link to="hero">{btn || 'Back to Top'}</Link>
         </span>
         <div className="social-links">
           {networks &&
@@ -31,7 +29,7 @@ const Footer = () => {
                   target="_blank"
                   aria-label={name}
                 >
-                  <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
+                  <i className={`fa fa-${name || 'refresh'}`} />
                 </a>
               );
             })}
--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,68 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Cullen | Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Cullen Wenzlick Portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'My name is ',
+  name: 'Cullen Wenzlick',
+  subtitle: 'Python Enthusiast // Software Developer // Game Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    "Coding Temple Graduate, Columbus State Community College Associate of Computer Science Student with a Game Developer Track.I'm a Python enthusiast with a passion for learning more languages. The more I learn, the more I can do. C# is next on my 'To Learn' list. Currently learning Godot Game Engine, and have experience using the Unity Game Engine",
+  paragraphTwo:
+    "My hobbies include music (I'm currently learning guitar/bass), playing video games (currently playing Halo: Master Chief Collection, Halo 5, Age of Empires 2, and Dark Souls 2.), and making video games (some nice people in my college class and I made a game called 'Gooey G'. Read on for more information!).",
+  paragraphThree: 'Located in Columbus, Ohio, United States',
+  resume: 'https://drive.google.com/file/d/1lzOTPN99wWd7q_MRs_GnYKW8UkbvbuAg/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
+  // {
+  //   id: nanoid(),
+  //   img: 'project.jpg',
+  //   title: '',
+  //   info: '',
+  //   info2: '',
+  //   url: '',
+  //   repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  // },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'drone.jpg',
+    title: 'Drone Inventory',
+    info: 'Using React, Flask, REST, and API calls, we are able to create new drones with information, update them, and delete them. You have to be signed-in in order to view any of the drones (and wait for the information to call and deliver).',
+    info2:
+      "This website is live on using Google's Firebase. Getting the API Key, comes from another week project that was uploaded to Heroku and uses a different folder of work (a different repo too).",
+    url: 'https://drone-inventory-r63-cw.firebaseapp.com/',
+    repo: 'https://github.com/Raenllanthos/drones_firebase', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'gooey.jpg',
+    title: 'Gooey G - Lead Programmer',
+    info: "A CSCC Game Project for students in their last semester of Spring 2021. A group of about 11 students created this story of a slime named Gooey G who has to escape a tower he's trapped in.",
+    info2:
+      'Being Lead Programmer, my role was to make sure all of the programmers were doing their job and to make sure of all of the necessary script pieces fit where they needed to be.',
+    url: 'https://cscc-game-projects.itch.io/gooey-g-sp2021',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: "Interested? Coffee's on me.",
+  btn: "Let's Talk",
+  email: 'cullen.wenzlick@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +72,27 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https:twitter.com/_cullenoscopy_',
     },
     {
       id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'itch-io',
+      url: 'https://raenllanthos.itch.io',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/cullen-wenzlick',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/Raenllanthos',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
