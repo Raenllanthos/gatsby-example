d5434816418d89a366b5c6fdf7deb6d6eee59fae
raenllanthos <cwdarksouls@gmail.com>
2021-08-03 14:33:49 -0400

    changed and added SHC

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -49,14 +49,16 @@ const Projects = () => {
                         </p>
                         <p className="mb-4">{info2 || ''}</p>
                       </div>
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="cta-btn cta-btn--hero"
-                        href={url || '#!'}
-                      >
-                        See Live
-                      </a>
+                      {url && (
+                        <a
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="cta-btn cta-btn--hero"
+                          href={url}
+                        >
+                          See Live
+                        </a>
+                      )}
 
                       {repo && (
                         <a
@@ -81,7 +83,7 @@ const Projects = () => {
                   >
                     <div className="project-wrapper__image">
                       <a
-                        href={url || '#!'}
+                        href={url || repo}
                         target="_blank"
                         aria-label="Project Link"
                         rel="noopener noreferrer"
--- src/mock/data.js
+++ src/mock/data.js
@@ -28,15 +28,16 @@ export const aboutData = {
 
 // PROJECTS DATA
 export const projectsData = [
-  // {
-  //   id: nanoid(),
-  //   img: 'superherocreator.jpg',
-  //   title: 'Super Hero Creator (WIP)',
-  //   info: 'Using React and Javascript, all users will be able to search for one hero, all heroes, or villans or heroes.',
-  //   info2: 'Users will need to be authenticated and authorized in order to Create, Update, and Delete the hereos that they make',
-  //   url: '',
-  //   repo: 'https://github.com/Raenllanthos/superherocreator', // if no repo, the button will not show up
-  // },
+  {
+    id: nanoid(),
+    img: 'superherocreator.jpg',
+    title: 'Super Hero Creator (WIP)',
+    info: 'Using React and Javascript, all users will be able to search for one hero, all heroes, or villans or heroes.',
+    info2:
+      'Users will need to be authenticated and authorized in order to Create, Update, and Delete the hereos that they make',
+    url: '', // if no url, the button will not show up
+    repo: 'https://github.com/Raenllanthos/superherocreator', // if no repo, the button will not show up
+  },
   {
     id: nanoid(),
     img: 'drone.jpg',
@@ -44,7 +45,7 @@ export const projectsData = [
     info: 'Using React, Flask, REST, and API calls, we are able to create new drones with information, update them, and delete them. You have to be signed-in in order to view any of the drones (and wait for the information to call and deliver).',
     info2:
       "This website is live on using Google's Firebase. Getting the API Key, comes from another week project that was uploaded to Heroku and uses a different folder of work (a different repo too).",
-    url: 'https://drone-inventory-r63-cw.firebaseapp.com/',
+    url: 'https://drone-inventory-r63-cw.firebaseapp.com/', // if no url, the button will not show up
     repo: 'https://github.com/Raenllanthos/drones_firebase', // if no repo, the button will not show up
   },
   {
@@ -54,7 +55,7 @@ export const projectsData = [
     info: "A CSCC Game Project for students in their last semester of Spring 2021. A group of about 11 students created this story of a slime named Gooey G who has to escape a tower he's trapped in.",
     info2:
       'Being Lead Programmer, my role was to make sure all of the programmers were doing their job and to make sure of all of the necessary script pieces fit where they needed to be.',
-    url: 'https://cscc-game-projects.itch.io/gooey-g-sp2021',
+    url: 'https://cscc-game-projects.itch.io/gooey-g-sp2021', // if no url, the button will not show up
     repo: '', // if no repo, the button will not show up
   },
 ];
